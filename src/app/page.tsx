415d55f8ea7de3ab5fbf7f74c743147b31039bca
kevinashley <kashley3141@gmail.com>
2024-07-14 21:00:41 -0700

    initial setup, homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,109 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React, { Fragment } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { ReactLogo, MuiLogo, NextLogo, VercelLogo } from "../components/logos";
+import Link from "next/link";
+const backgroundGradient = "rgba(0,0,0,0.7)";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const Home = () => {
+    return (
+        <Fragment>
+            <Box
+                sx={{
+                    color: "#fff",
+                    backgroundImage: `linear-gradient(0deg, ${backgroundGradient}, ${backgroundGradient}), url(/images/pexels-photo-12027143-cropped.jpg)`,
+                    width: "100%",
+                    minHeight: "28vw",
+                    backgroundSize: "cover",
+                    backgroundPosition: "bottom",
+                    padding: "2rem",
+                    textShadow: "0px 0px 5px black",
+                    display: "flex",
+                }}
+            >
+                <Box
+                    sx={{
+                        maxWidth: 700,
+                        margin: "auto",
+                        overflow: "hidden",
+                        textAlign: "center",
+                    }}
+                >
+                    <Typography
+                        variant="h1"
+                        component="h1"
+                        sx={{
+                            textAlign: "center",
+                            mt: 5,
+                            mb: 0.5,
+                            fontSize: {
+                                xs: "2rem",
+                                lg: "3rem",
+                            },
+                        }}
+                    >
+                        Kevin Ashley
+                    </Typography>
+                    <Typography
+                        component="p"
+                        sx={{
+                            fontSize: "12px",
+                            marginBottom: "3rem",
+                        }}
+                    >
+                        Sacramento, CA
+                    </Typography>
+                    <Typography
+                        component="p"
+                        sx={{
+                            marginBottom: "3rem",
+                        }}
+                    >
+                        Thanks for visiting my website. Check out my{" "}
+                        <Link href="/portfolio/">portfolio</Link> for examples
+                        of my work. All of the code for this website is
+                        available to review at github.
+                    </Typography>
+                    <Box sx={{ mb: 5 }}>
+                        {" "}
+                        <Button
+                            variant="contained"
+                            href="https://github.com/KevinAshley/kashley.net"
+                            target="_blank"
+                        >
+                            Github Repo
+                        </Button>
+                    </Box>
+                </Box>
+            </Box>
+            <Box
+                sx={{
+                    background: "gray",
+                    textAlign: "center",
+                    lineHeight: 0,
+                    padding: "0.5rem",
+                }}
+            >
+                <NextLogo />
+                <ReactLogo />
+                <MuiLogo />
+                <VercelLogo />
+            </Box>
+            <Typography
+                component="p"
+                sx={{
+                    fontSize: "12px",
+                    textAlign: "center",
+                    padding: "10px",
+                    borderBottom: "solid rgba(0,0,0,0.1) 1px",
+                }}
+            >
+                This React.js website was built with NextJS, Material UI
+                components, and&nbsp;Vercel hosting.
+            </Typography>
+        </Fragment>
+    );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
-}
+export default Home;
