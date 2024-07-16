2c5553132c7cce80516f7d505e56b0439e369b98
kevinashley <kashley3141@gmail.com>
2024-07-15 20:46:32 -0700

    added custom MUI theme provider

---
+++ src/components/themeProvider.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import {
+    createTheme,
+    ThemeProvider as MuiThemeProvider,
+} from "@mui/material/styles";
+
+let baseTheme = createTheme({
+    palette: {
+        primary: {
+            light: "#63ccff",
+            main: "#009be5",
+            dark: "#006db3",
+        },
+    },
+    typography: {
+        h1: {
+            fontSize: "2rem",
+        },
+        h5: {
+            fontWeight: 500,
+            fontSize: 26,
+            letterSpacing: 0.5,
+        },
+    },
+    shape: {
+        borderRadius: 8,
+    },
+    components: {
+        MuiTab: {
+            defaultProps: {
+                disableRipple: true,
+            },
+        },
+    },
+    mixins: {
+        toolbar: {
+            minHeight: 48,
+        },
+    },
+});
+
+export const theme = {
+    ...baseTheme,
+    components: {
+        MuiDrawer: {
+            styleOverrides: {
+                paper: {
+                    backgroundColor: "#081627",
+                },
+            },
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: "none",
+                },
+                contained: {
+                    boxShadow: "none",
+                    "&:active": {
+                        boxShadow: "none",
+                    },
+                },
+            },
+        },
+        MuiTabs: {
+            styleOverrides: {
+                root: {
+                    marginLeft: baseTheme.spacing(1),
+                },
+                indicator: {
+                    height: 3,
+                    borderTopLeftRadius: 3,
+                    borderTopRightRadius: 3,
+                    backgroundColor: baseTheme.palette.common.white,
+                },
+            },
+        },
+        MuiTab: {
+            styleOverrides: {
+                root: {
+                    textTransform: "none",
+                    margin: "0 16px",
+                    minWidth: 0,
+                    padding: 0,
+                    [baseTheme.breakpoints.up("md")]: {
+                        padding: 0,
+                        minWidth: 0,
+                    },
+                },
+            },
+        },
+        MuiIconButton: {
+            styleOverrides: {
+                root: {
+                    padding: baseTheme.spacing(1),
+                },
+            },
+        },
+        MuiTooltip: {
+            styleOverrides: {
+                tooltip: {
+                    borderRadius: 4,
+                },
+            },
+        },
+        MuiDivider: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: "rgb(255,255,255,0.15)",
+                },
+            },
+        },
+        MuiListItemButton: {
+            styleOverrides: {
+                root: {
+                    "&.Mui-selected": {
+                        color: "#4fc3f7",
+                    },
+                },
+            },
+        },
+        MuiListItemText: {
+            styleOverrides: {
+                primary: {
+                    fontSize: 14,
+                    fontWeight: baseTheme.typography.fontWeightMedium,
+                },
+            },
+        },
+        MuiListItemIcon: {
+            styleOverrides: {
+                root: {
+                    color: "inherit",
+                    minWidth: "auto",
+                    marginRight: baseTheme.spacing(2),
+                    "& svg": {
+                        fontSize: 20,
+                    },
+                },
+            },
+        },
+        MuiAvatar: {
+            styleOverrides: {
+                root: {
+                    width: 32,
+                    height: 32,
+                },
+            },
+        },
+    },
+};
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+    return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
+};
+
+export default ThemeProvider;
