ec9b397d99c06c063d26a453d61bd83ca3401178
kevinashley <kashley3141@gmail.com>
2024-07-15 20:45:03 -0700

    github linkout component

---
+++ src/components/githubLinkout.tsx
@@ -0,0 +1,27 @@
+/** @format */
+
+import React, { Fragment } from "react";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import { portfolioRoutes } from "@/routes";
+import { usePathname } from "next/navigation";
+
+const GithubLinkout = () => {
+    const pathname = usePathname();
+    const portfolioEntry = portfolioRoutes.find((item) =>
+        item.route.includes(pathname)
+    );
+    const githubUrl = portfolioEntry?.github;
+    if (!githubUrl) {
+        return <Fragment></Fragment>;
+    }
+    return (
+        <Box sx={{ textAlign: "center", my: 2 }}>
+            <Link href={githubUrl} target="_blank">
+                View Source Code
+            </Link>
+        </Box>
+    );
+};
+
+export default GithubLinkout;
