971ffd112e9b760b3a565802db1c252811340bcd
kevinashley <kashley3141@gmail.com>
2024-07-15 21:47:20 -0700

    theme context provider with localState

--- src/components/themeProvider.tsx
+++ src/components/themeProvider.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import React, {
+    Dispatch,
+    ReactNode,
+    SetStateAction,
+    createContext,
+    useState,
+} from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import {
     createTheme,
     ThemeProvider as MuiThemeProvider,
 } from "@mui/material/styles";
 
-const getTheme = (prefersDarkMode?: boolean) => {
+const getTheme = ({ darkMode }: { darkMode: boolean }) => {
     return createTheme({
         palette: {
-            mode: prefersDarkMode ? "dark" : "light",
+            mode: darkMode ? "dark" : "light",
             primary: {
                 light: "#63ccff",
                 main: "#009be5",
@@ -30,10 +36,42 @@ const getTheme = (prefersDarkMode?: boolean) => {
     });
 };
 
+interface ThemeContextProviderIf {
+    darkMode: boolean;
+    paletteMode: palleteModeType;
+    setPaletteMode: Dispatch<SetStateAction<palleteModeType>>;
+}
+
+export const ThemeContext = createContext<ThemeContextProviderIf>({
+    darkMode: false,
+    paletteMode: "auto",
+    setPaletteMode: () => {},
+});
+
+type palleteModeType = "auto" | "dark" | "light";
+
 const ThemeProvider = ({ children }: { children: ReactNode }) => {
     const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-    const theme = getTheme(prefersDarkMode);
-    return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
+    const [paletteMode, setPaletteMode] = useState<palleteModeType>("auto");
+
+    const darkMode =
+        paletteMode == "auto" ? prefersDarkMode : paletteMode == "dark";
+
+    const theme = getTheme({
+        darkMode,
+    });
+
+    const themeContextProviderValue: ThemeContextProviderIf = {
+        darkMode,
+        paletteMode,
+        setPaletteMode,
+    };
+
+    return (
+        <ThemeContext.Provider value={themeContextProviderValue}>
+            <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
+        </ThemeContext.Provider>
+    );
 };
 
 export default ThemeProvider;
