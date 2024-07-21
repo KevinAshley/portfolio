6cb59f75ad2df009ebf5fb3bb5cdff14944d5b75
kevinashley <kashley3141@gmail.com>
2024-07-21 13:45:19 -0700

    theme revision

--- src/theme.ts
+++ src/theme.ts
@@ -1,19 +1,9 @@
 "use client";
-import { Roboto } from "next/font/google";
+
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 import { grey } from "@mui/material/colors";
-// import { pink } from "@mui/material/colors";
-
-const roboto = Roboto({
-    weight: ["300", "400", "500", "700"],
-    subsets: ["latin"],
-    display: "swap",
-});
 
 const theme = extendTheme({
-    typography: {
-        fontFamily: roboto.style.fontFamily,
-    },
     colorSchemes: {
         light: {
             palette: {
