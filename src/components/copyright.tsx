3dbdf009ca558d0b2606b2e0319ecc58b94e5f3c
kevinashley <kashley3141@gmail.com>
2024-07-22 20:41:25 -0700

    revised footer copyright text

--- src/components/copyright.tsx
+++ src/components/copyright.tsx
@@ -1,15 +1,11 @@
-"use client";
-// client component so the date is always current
 import Typography from "@mui/material/Typography";
 import RoutedLink from "./routedLink";
 
 export default function Copyright() {
     return (
         <Typography variant="body2" color="#fff" align="center">
-            {"Copyright © "}
-            <RoutedLink color="inherit" href="/">
-                Kevin Ashley
-            </RoutedLink>{" "}
+            Kevin Ashley
+            {" © "}
             {new Date().getFullYear()}
         </Typography>
     );
