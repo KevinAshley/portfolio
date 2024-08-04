0d480379514bbec7478e3cb08958ad80fea9c0e1
kevinashley <kashley3141@gmail.com>
2024-08-04 15:42:07 -0700

    next config for docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    compiler: {
-        styledComponents: true,
-    },
+    reactStrictMode: true,
+    swcMinify: true,
+    // except for webpack, other parts are left as generated
+    // webpack: (config, context) => {
+    //     config.watchOptions = {
+    //         poll: 1000,
+    //         aggregateTimeout: 300,
+    //     };
+    //     return config;
+    // },
+    output: "standalone",
 };
 
 export default nextConfig;
