d7366239715cf52be29b98bd6c9215775b21bd18
kevinashley <kashley3141@gmail.com>
2024-08-04 17:20:51 -0700

    docker config and testing

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
     // except for webpack, other parts are left as generated
     // webpack: (config, context) => {
     //     config.watchOptions = {
