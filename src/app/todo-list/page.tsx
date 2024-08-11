ac69dbd2bdd04b3ea20080b88f07e99ac62d966b
kevinashley <kashley3141@gmail.com>
2024-08-10 23:23:26 -0700

    todo list progress

--- src/app/todo-list/page.tsx
+++ src/app/todo-list/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import DataTable, { TableColumnIf } from "@/components/dataTable";
+import ModalForm from "@/components/modalForm";
+import { InputIf } from "@/components/form";
 import Box from "@mui/material/Box";
 import { useState } from "react";
+
+const itemFormInputs: InputIf[] = [
+    {
+        type: "text",
+        label: "Name",
+        id: "name",
+        required: true,
+    },
+];
 
 const tableColumns: TableColumnIf[] = [
     {
@@ -14,18 +25,22 @@ const tableColumns: TableColumnIf[] = [
         id: "name",
     },
     {
-        label: "Email",
-        id: "email",
-    },
-    {
-        label: "Session Expires",
-        id: "session_expires",
-        date: true,
+        label: "Completed",
+        id: "completed",
     },
 ];
 
 const TodoList = () => {
     const [addNew, setAddNew] = useState(false);
+    const [newItemValues, setNewItemValues] = useState<{
+        [key: string]: string;
+    }>({});
+    const [selectedIds, setSelectedIds] = useState<number[]>([]);
+
+    const handleSubmit = () => {
+        console.log("handle submit!!!");
+    };
+
     //test
     return (
         <Box maxWidth="md" sx={{ margin: "auto", mt: 5 }}>
@@ -33,11 +48,20 @@ const TodoList = () => {
                 data={[]}
                 setAddNewOpen={setAddNew}
                 title={"To-Do List"}
-                selected={[]}
-                setSelected={() => {}}
+                selected={selectedIds}
+                setSelected={setSelectedIds}
                 deleteSelected={() => {}}
                 tableColumns={tableColumns}
                 defaultOrderBy={"id"}
+            />
+            <ModalForm
+                title={"Add New To-Do Item"}
+                open={addNew}
+                setOpen={setAddNew}
+                handleSubmit={handleSubmit}
+                inputs={itemFormInputs}
+                values={newItemValues}
+                setValues={setNewItemValues}
             />
         </Box>
     );
