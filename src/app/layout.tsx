39dfa2e1127101925e3f55970b5eb703243ccd40
kevinashley <kashley3141@gmail.com>
2024-08-17 13:53:30 -0700

    migrated more core components to sharedComponents

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,27 @@
 import type { Metadata } from "next";
-import PageWrapper from "@/components/pageWrapper";
+import PageWrapper from "@/sharedComponents/pageWrapper";
 import ThemeProvider from "@/sharedComponents/themeProvider";
 import MainContextProvider from "@/sharedComponents/contexts/mainContext";
 import Toast from "@/sharedComponents/toast";
+import { RouteGroupIf } from "@/sharedComponents/navigator";
 import theme from "../theme";
+import { mainRoutes, portfolioRoutes } from "../routes";
 
 export const metadata: Metadata = {
     title: "Kevin Ashley's Portfolio",
     description: "Kevin Ashley's full-stack web development portfolio",
 };
+
+const groupedRoutes: RouteGroupIf[] = [
+    {
+        routes: [...mainRoutes],
+    },
+    {
+        routes: [...portfolioRoutes],
+    },
+];
+
+const websiteName = "Kevin Ashley";
 
 export default function RootLayout({
     children,
@@ -20,7 +33,12 @@ export default function RootLayout({
             <body>
                 <ThemeProvider theme={theme}>
                     <MainContextProvider>
-                        <PageWrapper>{children}</PageWrapper>
+                        <PageWrapper
+                            groupedRoutes={groupedRoutes}
+                            websiteName={websiteName}
+                        >
+                            {children}
+                        </PageWrapper>
                         <Toast />
                     </MainContextProvider>
                 </ThemeProvider>
