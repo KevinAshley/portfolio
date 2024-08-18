1d88914d79369274bf8c8b7470c3e977f918850f
kevinashley <kashley3141@gmail.com>
2024-08-17 22:43:36 -0700

    DB migration testing

--- src/app/api/todo-list/route.ts
+++ src/app/api/todo-list/route.ts
@@ -12,7 +12,6 @@ export async function GET(req: Request) {
                 completed: true,
                 createdAt: true,
                 updatedAt: true,
-                test: true,
             },
         });
         return NextResponse.json(todoItems);
