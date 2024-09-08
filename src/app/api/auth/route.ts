18a81ca5596ac504c12c5a35e0b5867b0f3c8dcc
kevinashley <kashley3141@gmail.com>
2024-09-07 19:16:15 -0700

    more login / auth progress

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,7 +1,19 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { handleError } from "@/sharedComponents/nextApi";
+import { getUniqueIdString, handleError } from "@/sharedComponents/nextApi";
 import { cookies } from "next/headers";
+import {
+    createPasswordHash,
+    createSessionHash,
+} from "@/sharedComponents/nextApi/authentication";
+import jwt from "jsonwebtoken";
+import moment from "moment";
+const isProduction = process.env.NODE_ENV === "production";
+
+const jwtPrivateKey =
+    (isProduction
+        ? process.env.JWT_PRIVATE_KEY_PROD
+        : process.env.JWT_PRIVATE_KEY_DEV) || "jwt_private_key_fallback";
 
 const prisma = new PrismaClient();
 
@@ -27,7 +39,42 @@ export async function POST(req: Request) {
     try {
         // Log In
         // set the session token
-        cookies().set("sessionToken", "asdfasdf", {
+        const genericFailureMessage = "Invalid email or password";
+        const data = await req.json();
+        const { email, password } = data;
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+        if (!user || createPasswordHash(password) !== user.password) {
+            throw new Error(genericFailureMessage);
+        }
+
+        // create a sessionId, hash it, and save it to the db.
+        const sessionId = getUniqueIdString();
+        const userAgent = req.headers.get("User-Agent") || "User_Agent_Missing";
+        const ip = req.headers.get("X-Forwarded-For") || "IP_address_Missing";
+
+        await prisma.user.update({
+            where: {
+                id: user.id,
+            },
+            data: {
+                session_id: sessionId,
+                session_expires: moment().add(1, "days").toDate(),
+            },
+        });
+        // put the session ID in a JWT, and set in a cookie
+        const sessionToken = jwt.sign(
+            {
+                userId: user.id,
+                sessionHash: createSessionHash(sessionId, userAgent, ip),
+            },
+            jwtPrivateKey
+        );
+
+        cookies().set("sessionToken", sessionToken, {
             secure: true,
             maxAge: 86400000,
             httpOnly: true,
