3f103f837a7a306b3573e5271ef772b0ed242150
kevinashley <kashley3141@gmail.com>
2024-09-08 15:28:35 -0700

    middleware protection for admin routes

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,12 +2,13 @@ import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
     pages: {
-        signIn: "/login",
+        signIn: "/",
     },
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
-            console.log("HELLOOOOOOOO!!!! are we logged in???", auth?.user);
+            // console.log("middleware", nextUrl.pathname);
+            // console.log("HELLOOOOOOOO!!!! are we logged in???", auth?.user);
             // const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
             // if (isOnDashboard) {
             //     if (isLoggedIn) return true;
@@ -15,6 +16,9 @@ export const authConfig = {
             // } else if (isLoggedIn) {
             //     return Response.redirect(new URL("/dashboard", nextUrl));
             // }
+            if (nextUrl.pathname.startsWith("/admin/") && !isLoggedIn) {
+                return false;
+            }
             return true;
         },
     },
