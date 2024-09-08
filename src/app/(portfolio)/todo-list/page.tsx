a3e95acc4ad649720b098daeb7181ef376fd2df2
kevinashley <kashley3141@gmail.com>
2024-09-08 15:00:44 -0700

    datatable w generic placeholder

--- src/app/(portfolio)/todo-list/page.tsx
+++ src/app/(portfolio)/todo-list/page.tsx
@@ -1,13 +1,6 @@
-"use client";
-import { useContext } from "react";
 import { ColumnType, TableColumnIf } from "@/sharedComponents/dataTable";
 import { InputIf } from "@/sharedComponents/form";
 import DataTableWithModals from "@/sharedComponents/dataTableWithModals";
-import DataTable from "@/sharedComponents/dataTable";
-import { UserContext } from "@/sharedComponents/contexts/userContext";
-import Box from "@mui/material/Box";
-import ReportIcon from "@mui/icons-material/Report";
-import Typography from "@mui/material/Typography";
 import {
     getTodoItems,
     addTodoItem,
@@ -49,47 +42,6 @@ const tableColumns: TableColumnIf[] = [
 ];
 
 const TodoList = () => {
-    const { authenticating, user } = useContext(UserContext);
-
-    if (!user) {
-        return (
-            <DataTable
-                tableHeading={"To-Do List"}
-                tableColumns={tableColumns}
-                setAddNewOpen={() => {}}
-                data={[]}
-                selected={[]}
-                setSelected={() => {}}
-                deleteSelected={() => {}}
-                emptyRowsContent={
-                    <Box sx={{ position: "relative" }}>
-                        <Box
-                            sx={{
-                                position: "absolute",
-                                inset: 0,
-                                display: "flex",
-                                flexDirection: "column",
-                                alignItems: "center",
-                                justifyContent: "center",
-                            }}
-                        >
-                            <ReportIcon
-                                sx={{ fontSize: "10rem", marginBottom: "1rem" }}
-                            />
-                            <Typography variant={"h2"} component={"h2"} mb={2}>
-                                Login Required
-                            </Typography>
-                            <Box>
-                                Only logged-in users can use the To-Do List.
-                            </Box>
-                        </Box>
-                    </Box>
-                }
-                loading={authenticating}
-            />
-        );
-    }
-
     return (
         <DataTableWithModals
             tableHeading={"To-Do List"}
@@ -101,8 +53,6 @@ const TodoList = () => {
             editItem={editTodoItem}
             itemFormInputs={itemFormInputs}
             getItems={getTodoItems}
-            key={user?.id}
-            // ^ force the table to re-mount if the user changes
         />
     );
 };
