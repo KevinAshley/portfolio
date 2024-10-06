d066bed383cef4a6dbbc6d6314ba1d564f159bb4
kevinashley <kashley3141@gmail.com>
2024-10-05 21:17:47 -0700

    projects grid improvements - display as cards

--- src/components/projectsGrid.tsx
+++ src/components/projectsGrid.tsx
@@ -1,15 +1,34 @@
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import Tooltip from "@mui/material/Tooltip";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
 
 const projects = [
     {
         label: "iHOUSEweb",
+        caption: "2015 - 2023",
         link: "https://www.ihouseweb.com",
+        description: (
+            <span>
+                A website hosting company specializing in bespoke websites for
+                Real&nbsp;Estate&nbsp;Agents.
+            </span>
+        ),
     },
     {
         label: "One Day Doors & Closets",
+        caption: "2023 - Present",
         link: "https://onedaydoorsandclosets.com",
+        description: (
+            <span>
+                A manufacturing and installation company specializing in door
+                replacement and closet organizers.
+            </span>
+        ),
     },
 ];
 
@@ -22,55 +41,70 @@ const ProjectsGrid = () => {
                 [theme.breakpoints.up("md")]: {
                     gridTemplateColumns: "1fr 1fr",
                 },
-                alignItems: "center",
+                alignItems: "flex-start",
                 justifyContent: "center",
                 gap: "2rem",
             })}
         >
             {projects.map((project, index) => {
                 return (
-                    <Box key={index}>
-                        <Box
-                            sx={{
-                                aspectRatio: "2 / 1",
-                                textAlign: "center",
-                                display: "flex",
-                                alignItems: "center",
-                                justifyContent: "center",
-                                position: "relative",
-                                "& > iframe": {
-                                    position: "absolute",
-                                    width: "300%",
-                                    height: "300%",
-                                    transform: "scale(0.33)",
-                                },
-                            }}
-                        >
-                            <iframe src={project.link} />
-                            <Link
-                                href={project.link}
-                                color={"inherit"}
-                                target={"_blank"}
-                                sx={{
-                                    position: "absolute",
-                                    inset: 0,
-                                    textDecoration: "none",
-                                }}
-                            >
-                                &nbsp;
-                            </Link>
-                        </Box>
-                        <Link
+                    <Card elevation={5} key={index} sx={{ height: "100%" }}>
+                        <CardActionArea
                             href={project.link}
                             target={"_blank"}
-                            sx={{
-                                display: "block",
-                                marginTop: "1rem",
-                            }}
+                            sx={{ height: "100%" }}
                         >
-                            {project.label}
-                        </Link>
-                    </Box>
+                            <Box
+                                sx={{
+                                    aspectRatio: "2 / 1",
+                                    textAlign: "center",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    position: "relative",
+                                    "& > iframe": {
+                                        position: "absolute",
+                                        width: "300%",
+                                        height: "300%",
+                                        transform: "scale(0.33334)",
+                                        border: "none",
+                                        opacity: 0.99,
+                                    },
+                                }}
+                            >
+                                <iframe src={project.link} />
+                                <Box
+                                    // covers the iframe
+                                    sx={{
+                                        position: "absolute",
+                                        inset: 0,
+                                        background:
+                                            "linear-gradient(180deg,  transparent, rgba(0,0,0,0.3))",
+                                    }}
+                                >
+                                    &nbsp;
+                                </Box>
+                            </Box>
+                            <CardContent>
+                                <Typography variant="h5" component="h3">
+                                    {project.label}
+                                </Typography>
+                                <Typography
+                                    gutterBottom
+                                    variant={"caption"}
+                                    component={"div"}
+                                >
+                                    {project.caption}
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    sx={{ color: "text.secondary" }}
+                                >
+                                    {project.description}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
                 );
             })}
         </Box>
