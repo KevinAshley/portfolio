8d9401ae3523df4e5592a064e691ccecbe1de7cb
kevinashley <kashley3141@gmail.com>
2024-10-05 23:01:41 -0700

    defer loading of iframes - wait until they are in the viewport

--- src/components/projectsGrid.tsx
+++ src/components/projectsGrid.tsx
@@ -3,6 +3,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
+import { useEffect, useRef, useState } from "react";
 
 const projects = [
     {
@@ -30,6 +31,47 @@ const projects = [
 ];
 
 const ProjectsGrid = () => {
+    const [loadIframes, setLoadIframes] = useState(false);
+    const [revealedIframes, setRevealedIframes] = useState<number[]>([]);
+    const observerRef = useRef();
+
+    useEffect(() => {
+        // We don't want the iframes to load until they enter the viewport!
+        const callbackFunction = (entries: any) => {
+            const [entry] = entries;
+            const isIntersecting = entry.isIntersecting;
+            if (isIntersecting) {
+                setLoadIframes(true);
+            }
+        };
+        const observedElement = observerRef.current;
+        const observer = new IntersectionObserver(callbackFunction, {
+            root: null,
+            rootMargin: "1px",
+            threshold: 0.1,
+        });
+        if (observedElement) {
+            observer.observe(observedElement);
+        }
+        return () => {
+            if (observedElement) {
+                observer.unobserve(observedElement);
+            }
+        };
+    }, []);
+
+    const handleIframeLoaded = (index: number) => {
+        if (index == 0) {
+            setTimeout(() => {
+                // This is an ugly hack, to avoid a scroll position jump when the
+                // chat widget inside the iHOUSEweb site loads in.
+                setRevealedIframes((prev) => [...prev, index]);
+            }, 7000);
+        } else {
+            setRevealedIframes((prev) => [...prev, index]);
+        }
+    };
+
     return (
         <Box
             sx={(theme) => ({
@@ -42,8 +84,10 @@ const ProjectsGrid = () => {
                 justifyContent: "center",
                 gap: "2rem",
             })}
+            ref={observerRef}
         >
             {projects.map((project, index) => {
+                const iframeIsRevealed = revealedIframes.includes(index);
                 return (
                     <Card elevation={5} key={index} sx={{ height: "100%" }}>
                         <CardActionArea
@@ -65,11 +109,19 @@ const ProjectsGrid = () => {
                                         height: "300%",
                                         transform: "scale(0.33334)",
                                         border: "none",
-                                        opacity: 0.99,
+                                        pointerEvents: "none",
+                                        display: iframeIsRevealed
+                                            ? "block"
+                                            : "none",
                                     },
                                 }}
                             >
-                                <iframe src={project.link} />
+                                {loadIframes && (
+                                    <iframe
+                                        src={project.link}
+                                        onLoad={() => handleIframeLoaded(index)}
+                                    />
+                                )}
                                 <Box
                                     // covers the iframe
                                     sx={{
